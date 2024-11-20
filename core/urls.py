f4cd63dddf15b5394ce689f695dc395d684d914d
AShamsudinov <AShamsudinov@beeline.kg>
2024-11-20 13:58:50 +0600

    added jwt authentication

--- core/urls.py
+++ core/urls.py
@@ -15,13 +15,24 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from core.swagger import urlpatterns as swagger_yasg
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+api_patterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('api/v1/', include(api_patterns)),
 ] + swagger_yasg
 
 
